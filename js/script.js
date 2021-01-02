d0efdaa7518f8775adc50bee2ff6d1f4446725b7
laclieuquy <71786283+laclieuquy@users.noreply.github.com>
2021-01-03 02:10:37 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-function Redirect() {
-    window.location="http://lamsaodecode.epizy.com/";
+var time = 12; //How long (in seconds) to countdown
+var page = "http://lamsaodecode.epizy.com/"; //The page to redirect to
+function countDown(){
+    time--;
+    gett("container").innerHTML = time;
+    if(time == -1){
+        window.location = page;
+    }
 }
-setTimeout('Redirect()', 12000);
+function gett(id){
+    if(document.getElementById) return document.getElementById(id);
+        if(document.all) return document.all.id;
+        if(document.layers) return document.layers.id;
+        if(window.opera) return window.opera.id;
+    }
+    function init(){
+        if(gett('container')){
+        setInterval(countDown, 1000);
+        gett("container").innerHTML = time;
+    }
+    else{
+        setTimeout(init, 50);
+    }
+}
+document.onload = init();
