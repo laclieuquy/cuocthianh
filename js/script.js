5904f09082ae8aaaf378983c2fdd762607c601f8
laclieuquy <71786283+laclieuquy@users.noreply.github.com>
2021-01-03 15:27:05 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,25 +1,4 @@
-var time = 10; //How long (in seconds) to countdown
-var page = "http://lamsaodecode.epizy.com/"; //The page to redirect to
-function countDown(){
-    time--;
-    gett("container").innerHTML = time;
-    if(time == -1){
-        window.location = page;
-    }
+function chuyenhuong() {
+    window.location = "http://whitehat.42web.io/";
 }
-function gett(id){
-    if(document.getElementById) return document.getElementById(id);
-        if(document.all) return document.all.id;
-        if(document.layers) return document.layers.id;
-        if(window.opera) return window.opera.id;
-    }
-    function init(){
-        if(gett('container')){
-        setInterval(countDown, 1000);
-        gett("container").innerHTML = time;
-    }
-    else{
-        setTimeout(init, 50);
-    }
-}
-document.onload = init();
+setTimeout("chuyenhuong()",10000);
